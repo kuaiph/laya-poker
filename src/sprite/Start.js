bfb9c7517dd06263bac6029e972746ca73224a8a
cheneyweb <457299596@qq.com>
2018-11-07 00:13:03 +0800

    占位移动

--- src/sprite/Start.js
+++ src/sprite/Start.js
@@ -6,7 +6,7 @@ export default class Start extends Laya.Script {
     }
     onClick() {
         let userId = 223456
-        WebSocket.send({ method: 'JOIN_TABLE', userId }).then((res) => {
+        WebSocket.send({ method: 'ROUND_BEGIN', userId }).then((res) => {
             if (!res.err) {
                 WebSocket.globalData = { round: res.round, isBegin: false }
                 Laya.Scene.open('Game.scene')
